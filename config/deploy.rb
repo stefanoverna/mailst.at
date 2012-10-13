6a91132fca67ff10b70f66d301baa9d27f89d7ab
Stefano Verna <stefano.verna@welaika.com>
2012-10-13 11:32:32 +0200

    Compile assets only when needed

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,3 +87,16 @@ deploy.task :restart, :roles => :app do
   # Restart Application
   run "touch #{current_path}/tmp/restart.txt"
 end
+
+namespace :deploy do
+    namespace :assets do
+      task :precompile, :roles => :web, :except => { :no_release => true } do
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
+    end
+  end
+end
