d3adefd96179e07ab985d02e424dec0825bca3d8
Stefano Verna <stefano.verna@welaika.com>
2012-10-13 08:45:04 +0200

    First commit :metal:

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem 'slim-rails'
+gem 'devise'
+gem 'simple_form'
+gem 'responders'
+gem 'cancan'
+gem 'inherited_resources'
+gem 'sextant'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'compass-rails'
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailstat::Application.config.secret_token = '504877fdb393f3a234fa8fca61ed651507f1dfe8c60dda3159e8efb2d290eef10f041004cdad60935ee21730c7f2f9b9093050ee15c71cc0a6caeb0d50fa8cf5'
