a86a4ffc02792d114cd02a1df198cf300c25381d
Fabrizio Monti <fabrizio.monti@welaika.com>
2012-10-14 23:15:46 +0200

    fix report sender class

--- app/services/reports_sender.rb
+++ app/services/reports_sender.rb
@@ -1,8 +1,13 @@
 class ReportsSender
   def self.send!
     Mailbox.with_report_to_be_sent.each do |mailbox|
-      MailboxDataFetcher.fetch_mailbox(mailbox)
-      mailbox.send_report!
+      begin
+        MailboxDataFetcher.fetch_mailbox(mailbox)
+        mailbox.send_report!
+        # mailbox.update_attribute(:last_report_sent_at, Time.now)
+      rescue Exception => e
+        puts "fallito invio a #{mailbox.username}:\t#{e.class} - #{e.message}"
+      end
     end
   end
 end
