ee8de706fd1c3b8c0ec0ffa0f4823b1bb7b5a1f2
Fabrizio Monti <fabrizio.monti@welaika.com>
2012-10-14 23:47:46 +0200

    spam always in development

--- app/services/reports_sender.rb
+++ app/services/reports_sender.rb
@@ -1,10 +1,16 @@
 class ReportsSender
   def self.send!
-    Mailbox.with_report_to_be_sent.each do |mailbox|
+    mailboxes = if Rails.env.production?
+                  Mailbox.with_report_to_be_sent
+                else
+                  Mailbox.all
+                end
+
+    mailboxes.each do |mailbox|
       begin
         MailboxDataFetcher.fetch_mailbox(mailbox)
         mailbox.send_report!
-        # mailbox.update_attribute(:last_report_sent_at, Time.now)
+        mailbox.update_attribute(:last_report_sent_at, Time.now)
       rescue Exception => e
         puts "fallito invio a #{mailbox.username}:\t#{e.class} - #{e.message}"
       end
