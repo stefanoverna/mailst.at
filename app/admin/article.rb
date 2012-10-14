e875011cbe15615de6065e4969382a4ae4d75357
Stefano Verna <stefano.verna@welaika.com>
2012-10-14 23:08:37 +0200

    Suggestions done

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -8,11 +8,7 @@ ActiveAdmin.register Article do
   form do |f|
     f.inputs "Details" do
       f.input :title
-      f.input :description, as: :wysihtml5, input_html: {
-        toolbar: {
-          commands: [ :bold, :italic, :link, :unordered_list, :ordered_list, :source ],
-          blocks: [ :h1, :h2, :h3, :p ]
-      }}
+      f.input :description
       f.input :published_at
     end
     f.buttons
--- app/admin/tip.rb
+++ app/admin/tip.rb
@@ -7,11 +7,7 @@ ActiveAdmin.register Tip do
   form do |f|
     f.inputs "Details" do
       f.input :title
-      f.input :description, as: :wysihtml5, input_html: {
-        toolbar: {
-          commands: [ :bold, :italic, :link, :unordered_list, :ordered_list, :source ],
-          blocks: [ :h1, :h2, :h3, :p ]
-      }}
+      f.input :description
       f.input :citation
     end
     f.buttons
